afa6fa799a39c57ce117391f7286314c24f43239
liuyang <1071992409@qq.com>
2019-03-02 17:39:10 +0800

    fibonacci array

--- 7斐波那契数列/fib.py
+++ 7斐波那契数列/fib.py
@@ -1,1 +1,19 @@
 # 求fib第n项
+# 思路：生成一个39项的数字，从前往后遍历生成
+
+# -*- coding:utf-8 -*-
+class Solution:
+    def Fibonacci(self, n):
+        # write code here
+        l = [0 for i in range(n+1)]
+        l[0] = 0
+        l[1] = 1
+        i = 2
+        while i <= n:
+            l[i] = l[i - 1] + l[i - 2]
+            i = i + 1
+
+        return l[n]
+
+s = Solution()
+print(s.Fibonacci(39))
